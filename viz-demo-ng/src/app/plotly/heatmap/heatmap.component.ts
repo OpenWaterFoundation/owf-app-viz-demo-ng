ef6b53944187ef4650656ebbc2ff3c6e2415b3bb
unknown <1gonzalez.sofia@gmail.com>
2020-06-26 13:45:23 -0600

    updated heatmap to anotated heatmap example

--- viz-demo-ng/src/app/plotly/heatmap/heatmap.component.ts
+++ viz-demo-ng/src/app/plotly/heatmap/heatmap.component.ts
@@ -17,17 +17,87 @@ export class HeatmapComponent implements OnInit {
   basicHeatmap(){
     const element = document.getElementById("chart") as HTMLDivElement;
 
-    var data = [
-      {
-        z: [[1, null, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
-        x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-        y: ['Morning', 'Afternoon', 'Evening'],
-        type: 'heatmap',
-        hoverongaps: false
-      }
-    ];
+    // var data = [
+    //   {
+    //     z: [[1, null, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+    //     x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    //     y: ['Morning', 'Afternoon', 'Evening'],
+    //     type: 'heatmap',
+    //     hoverongaps: false
+    //   }
+    // ];
+    var xValues = ['A', 'B', 'C', 'D', 'E'];
+
+var yValues = ['W', 'X', 'Y', 'Z'];
+
+var zValues = [
+  [0.00, 0.00, 0.75, 0.75, 0.00],
+  [0.00, 0.00, 0.75, 0.75, 0.00],
+  [0.75, 0.75, 0.75, 0.75, 0.75],
+  [0.00, 0.00, 0.00, 0.75, 0.00]
+];
+
+var colorscaleValue = [
+  [0, '#3D9970'],
+  [1, '#001f3f']
+];
+
+var data = [{
+  x: xValues,
+  y: yValues,
+  z: zValues,
+  type: 'heatmap',
+  colorscale: colorscaleValue,
+  showscale: false
+}];
+
+var layout = {
+  title: 'Annotated Heatmap',
+  annotations: [],
+  xaxis: {
+    ticks: '',
+    side: 'top'
+  },
+  yaxis: {
+    ticks: '',
+    ticksuffix: ' ',
+    width: 700,
+    height: 700,
+    autosize: false
+  }
+};
+
+for ( var i = 0; i < yValues.length; i++ ) {
+  for ( var j = 0; j < xValues.length; j++ ) {
+    var currentValue = zValues[i][j];
+    if (currentValue != 0.0) {
+      var textColor = 'white';
+    }else{
+      var textColor = 'black';
+    }
+    var result = {
+      xref: 'x1',
+      yref: 'y1',
+      x: xValues[j],
+      y: yValues[i],
+      text: zValues[i][j],
+      font: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(50, 171, 96)'
+      },
+      showarrow: false,
+      // font: {
+      //   color: textColor
+      // }
+    };
+    layout.annotations.push(result);
+  }
+}
+
+// Plotly.newPlot('myDiv', data, layout);
     
-    Plotly.newPlot(element, data);
+    Plotly.newPlot(element, data, layout);
   }
 }
 
